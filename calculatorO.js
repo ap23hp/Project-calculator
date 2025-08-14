9773b7d0e1f3a651fe6033f44f9a2a957b970e01
Apra <apra.khanna2304@gmail.com>
2025-08-14 14:59:25 +0530

    added buttons to html, added styles to buttons

--- calculatorO.js
+++ calculatorO.js
@@ -1,3 +1,8 @@
+let num1;
+let num2
+let operator;
+const anyButton=document.querySelectorAll('.btn')
+
 
 const add = function (a, b) {
   return a + b;
@@ -24,3 +29,27 @@ console.log(add(2,3))
 console.log(subtract(2,3))
 console.log(multiply(2,3))
 console.log(divide(3,3))
+
+const operate=function(num1,num2,operator){
+if(operator==="+"){
+    return add(num1,num2)
+}
+if(operator==="-"){
+    return subtract(num1,num2)
+}
+if(operator==="*"){
+    return multiply(num1,num2)
+}
+if(operator==="/"){
+    return divide(num1,num2)
+}
+
+}
+
+
+anyButton.forEach((btn)=>{
+    return btn.addEventListener("click",function(){
+        console.log(`button is clicked`)
+        console.log(typeof(btn.textContent))
+    })
+})
