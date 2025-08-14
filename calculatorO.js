c18a81406dc98e328fd200af6450804e6d57d6b2
Apra <apra.khanna2304@gmail.com>
2025-08-14 13:49:19 +0530

    added functions for operations

---
+++ calculatorO.js
@@ -0,0 +1,26 @@
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+
+// const multiply = function (...arg) {
+//   return arg.reduce((acc,curr)=>acc*curr,1);
+// };
+
+const multiply=function(a,b){
+    return a*b
+}
+
+const divide=function(a,b){
+    return a/b
+}
+
+console.log(add(2,3))
+console.log(subtract(2,3))
+console.log(multiply(2,3))
+console.log(divide(3,3))
